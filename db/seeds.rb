6cb1d6cfc46b5e412efe6321eae9933a2a8ad9a0
Yuki Atarashi <c8bji5ma@gmail.com>
2026-08-26 17:14:52 +0900

    Exclude seed data from test environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,14 @@
-priorities = ['低', '中', '高']
-statuses = ['未着手', '着手中', '完了']
+unless Rails.env.test?
+  priorities = ['低', '中', '高']
+  statuses = ['未着手', '着手中', '完了']
 
-50.times do |n|
-  Task.create!(
-    title: "タスク#{n + 1}",
-    content: "タスク#{n + 1}の内容",
-    deadline_on: Date.current + n.days,
-    priority: priorities[n % priorities.length],
-    status: statuses[n % statuses.length]
-  )
+  50.times do |n|
+    Task.create!(
+      title: "タスク#{n + 1}",
+      content: "タスク#{n + 1}の内容",
+      deadline_on: Date.current + n.days,
+      priority: priorities[n % priorities.length],
+      status: statuses[n % statuses.length]
+    )
+  end
 end
